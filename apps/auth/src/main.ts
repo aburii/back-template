d0322b19b342c7515958832939550745cea4ae41
aburii <jolan.brizard@epitech.eu>
2022-11-14 17:01:39 +0100

    feature(init): repository initialized

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { boostrapApp } from '@app/nest-common';
+
+async function bootstrap() {
+  boostrapApp()
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();

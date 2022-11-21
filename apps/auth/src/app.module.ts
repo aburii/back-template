d8fc072daaa77dad502fd10197b92574a7dc47e1
aburii <jolan.brizard@epitech.eu>
2022-11-21 17:37:51 +0100

    style(import): remove unused import

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './app.controller';
 import { AuthService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { config } from 'config'
 import { DatabaseModule } from '@app/database';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [

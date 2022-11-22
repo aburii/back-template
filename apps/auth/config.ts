86a929a84df9d5e3b171f82b4084930934fd40e2
aburii <jolan.brizard@epitech.eu>
2022-11-22 20:37:50 +0100

    refactor(database): Switch to mysql

--- apps/auth/config.ts
+++ apps/auth/config.ts
@@ -9,7 +9,7 @@ export const config = () => ({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     name: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PWD,
   }
 })

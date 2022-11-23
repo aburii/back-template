4efb6f40996f4524614646cbee259c48007477a2
aburii <jolan.brizard@epitech.eu>
2022-11-23 20:14:55 +0100

    refactor(methods): Remove unused methods

--- libs/user/src/user.repository.ts
+++ libs/user/src/user.repository.ts
@@ -1,4 +1,4 @@
-import { DataSource, DeleteResult, InsertResult, Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { Injectable } from '@nestjs/common';
 
@@ -6,21 +6,5 @@ import { Injectable } from '@nestjs/common';
 export class UserRepository extends Repository<User> {
   constructor(private readonly dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
-  }
-
-  async findAllUsers(): Promise<Array<User>> {
-    return await this.find()
-  }
-
-  async findUserById(id: number): Promise<User> {
-    return await this.findOneBy({id: id});
-  }
-
-  async insertUser(email: string, password: string): Promise<InsertResult> {
-    return await this.insert({email, password});
-  }
-
-  async deleteUserById(id: number): Promise<DeleteResult> {
-    return await this.delete(id)
   }
 }
